86ebc8f78b2b2163eda30d960876c6692eb0bf55
partadox <arta.kusuma07@gmail.com>
2023-06-15 13:03:50 +0800

    0.6

--- app/Controllers/API/Flip.php
+++ app/Controllers/API/Flip.php
@@ -57,209 +57,248 @@ class Flip extends ResourceController
         $this->bill                 = new Model_bill;
         $this->db 			        = \Config\Database::connect();
 
-        // Get the JSON data from the request body
-        // $json       = $this->request->getJSON();
-        // $jsonString = json_encode($json, JSON_PRETTY_PRINT);
-
         $request = \Config\Services::request();
 
         // get JSON data
-        // $jsonData = $request->getJSON(true);  // returns assoc array if valid JSON was sent
+        // $Data = $request->getJSON(true);  // returns assoc array if valid JSON was sent
         // if ($jsonData) {
         //     return $this->respondCreated(['message' => "Using JSON".$jsonData]);
-        // } else {
-        //     // not valid JSON, check if it's a form POST
-        //     $postData = $request->getPost();
-        //     if ($postData) {
-        //         return $this->respondCreated(['message' => "Using POST".$postData]);
-        //     } else {
-        //         return $this->respondCreated(['message' => "No JSON or POST data received."]);
-        //     }
-        // }
-        $postData = $request->getPost();
-            if ($postData) {
-                return $this->respondCreated(['message' => $postData]);
+        // } 
+        $Data = $request->getPost();
+        $message = $Data['message'];
+        $data = json_decode($message['data'], true);  // decode the JSON string into an associative array
+        if ($data) {
+            extract($data);  // Extract values from associative array
+            //Result
+            // 'id' => $id,
+            // 'bill_link_id' => $bill_link_id,
+            // 'bill_link' => $bill_link,
+            // 'bill_title' => $bill_title,
+            // 'sender_name' => $sender_name,
+            // 'sender_email' => $sender_email,
+            // 'sender_bank' => $sender_bank,
+            // 'sender_bank_type' => $sender_bank_type,
+            // 'amount' => $amount,
+            // 'status' => $status,
+            // 'created_at' => $created_at,
+            
+        } else {
+            return $this->respondCreated(['message' => "Invalid JSON data"]);
+        }
+
+        if ($status == 'SUCCESSFUL') {
+            $title_explode      = explode("-", $bill_title);
+        
+            $peserta_kelas_id   = $title_explode[0];
+            $cart_id            = $title_explode[1];
+            $bayar_id           = $title_explode[2];
+            $peserta_kelas      = $this->peserta_kelas->find($peserta_kelas_id);
+            $peserta_id         = $peserta_kelas['data_peserta_id'];
+            $kelas_id           = $peserta_kelas['data_kelas_id'];
+
+            $data_kelas         = $this->kelas->find($kelas_id);
+            $peserta            = $this->peserta->find($peserta_id);
+
+            $this->db->transStart();
+            $updateBayar = [
+                'status_bayar'              => 'Lunas',
+                'status_bayar_admin'        => 'SESUAI BAYAR',
+                'status_konfirmasi'         => 'Terkonfirmasi',
+                'tgl_bayar_konfirmasi'      => date("Y-m-d"),
+                'waktu_bayar_konfirmasi'    => date("H:i:s"),
+                'validator'                 => 'Flip Payment Gateway',
+            ];
+            $this->bayar->update($bayar_id, $updateBayar);
+
+            $bayar  = $this->bayar->find($bayar_id);
+            $daftar = $bayar['awal_bayar_daftar'];
+            $modul  = $bayar['awal_bayar_modul'];
+            $spp1   = $bayar['awal_bayar_spp1'];
+            $spp2   = $bayar['awal_bayar_spp2'];
+            $spp3   = $bayar['awal_bayar_spp3'];
+            $spp4   = $bayar['awal_bayar_spp4'];
+            $infaq  = $bayar['awal_bayar_infaq'];
+            $lainnya= $bayar['awal_bayar_lainnya'];
+
+            if ($daftar != '0') {
+                $dataabsen = [
+                    'bckp_absen_peserta_id'     => $peserta_id,
+                    'bckp_absen_peserta_kelas'  => $kelas_id,
+                ];
+                $this->absen_peserta->insert($dataabsen);
+    
+                $dataujian = [
+                    'bckp_ujian_peserta'     => $peserta_id,
+                    'bckp_ujian_kelas'       => $kelas_id,
+                ];
+                $this->ujian->insert($dataujian);
+                $PKdaftar = [
+                    'byr_daftar'            => $daftar,
+                    'dt_konfirmasi_daftar'  => date('Y-m-d H:i:s'),
+                    'data_absen'            => $this->absen_peserta->insertID(),
+                    'data_ujian'            => $this->ujian->insertID(),
+                    'expired_tgl_daftar'    => NULL,
+                    'expired_waktu_daftar'  => NULL,
+                ];
+                $this->peserta_kelas->update($peserta_kelas_id, $PKdaftar);
             }
 
-        
-        //Access the individual data fields from the JSON
-        // $id                 = $json->id;
-        // $bill_link          = $json->bill_link;
-        // $bill_link_id       = $json->bill_link_id;
-        // $bill_title         = $json->bill_title;
-        // $sender_name        = $json->sender_name;
-        // $sender_bank        = $json->sender_bank;
-        // $amount             = $json->amount;
-        // $status             = $json->status;
-        // $sender_bank_type   = $json->sender_bank_type;
-        // $created_at         = $json->created_at;
+            if ($spp1 != '0') {
+                $PKspp1 = [
+                    'byr_spp1'            => $spp1,
+                    'dt_konfirmasi_spp1'  => date('Y-m-d H:i:s')
+                ];
+                $this->peserta_kelas->update($peserta_kelas_id, $PKspp1);
+            }
 
-        // if ($status == 'SUCCESSFUL') {
-        //     $title_explode      = explode("-", $bill_title);
-        
-        //     $peserta_kelas_id   = $title_explode[0];
-        //     $cart_id            = $title_explode[1];
-        //     $bayar_id           = $title_explode[2];
-        //     $peserta_kelas      = $this->peserta_kelas->find($peserta_kelas_id);
-        //     $peserta_id         = $peserta_kelas['data_peserta_id'];
-        //     $kelas_id           = $peserta_kelas['data_kelas_id'];
+            if ($spp2 != '0') {
+                $PKspp2 = [
+                    'byr_spp2'            => $spp2,
+                    'dt_konfirmasi_spp2'  => date('Y-m-d H:i:s')
+                ];
+                $this->peserta_kelas->update($peserta_kelas_id, $PKspp2);
+            }
 
-        //     $data_kelas         = $this->kelas->find($kelas_id);
-        //     $peserta            = $this->peserta->find($peserta_id);
+            if ($spp3 != '0') {
+                $PKspp3 = [
+                    'byr_spp3'            => $spp3,
+                    'dt_konfirmasi_spp3'  => date('Y-m-d H:i:s')
+                ];
+                $this->peserta_kelas->update($peserta_kelas_id, $PKspp3);
+            }
 
-        //     $this->db->transStart();
-        //     $updateBayar = [
-        //         'status_bayar'              => 'Lunas',
-        //         'status_bayar_admin'        => 'SESUAI BAYAR',
-        //         'status_konfirmasi'         => 'Terkonfirmasi',
-        //         'tgl_bayar_konfirmasi'      => date("Y-m-d"),
-        //         'waktu_bayar_konfirmasi'    => date("H:i:s"),
-        //         'validator'                 => 'Flip Payment Gateway',
-        //     ];
-        //     $this->bayar->update($bayar_id, $updateBayar);
+            if ($spp4 != '0') {
+                $PKspp4 = [
+                    'byr_spp4'            => $spp4,
+                    'dt_konfirmasi_spp4'  => date('Y-m-d H:i:s')
+                ];
+                $this->peserta_kelas->update($peserta_kelas_id, $PKspp4);
+            }
 
-        //     $bayar  = $this->bayar->find($bayar_id);
-        //     $daftar = $bayar['awal_bayar_daftar'];
-        //     $modul  = $bayar['awal_bayar_modul'];
-        //     $spp1   = $bayar['awal_bayar_spp1'];
-        //     $spp2   = $bayar['awal_bayar_spp2'];
-        //     $spp3   = $bayar['awal_bayar_spp3'];
-        //     $spp4   = $bayar['awal_bayar_spp4'];
-        //     $infaq  = $bayar['awal_bayar_infaq'];
-        //     $lainnya= $bayar['awal_bayar_lainnya'];
+            if ($modul != '0') {
+                $PKmodul = [
+                    'byr_modul'            => $modul,
+                ];
+                $this->peserta_kelas->update($peserta_kelas_id, $PKmodul);
+                $data_modul = [
+                    'bayar_modul_id'        => $bayar_id,
+                    'bayar_modul'           => $modul,
+                    'status_bayar_modul'    => 'Lunas',
+                ];
+                $this->bayar_modul->insert($data_modul);
+            }
 
-        //     $dt_konfirmasi_daftar = date('Y-m-d H:i:s');
-        //     $dt_konfirmasi_spp2 = date('Y-m-d H:i:s');
-        //     $dt_konfirmasi_spp3 = date('Y-m-d H:i:s');
-        //     $dt_konfirmasi_spp4 = date('Y-m-d H:i:s');
+            if ($lainnya != '0') {
+                $data_lain = [
+                    'lainnya_bayar_id'        => $bayar_id,
+                    'bayar_lainnya'           => $lainnya,
+                    'data_peserta_id_lain'    => $peserta_id,
+                    'status_bayar_lainnya'    => 'Lunas',
+                ];
+                $this->bayar_lain->insert($data_lain);
+            }
 
-        //     if ($modul == '0') {
-        //         $modul = NULL;
-        //     }
+            if ($infaq != '0') {
+                $data_infaq = [
+                    'infaq_bayar_id'        => $bayar_id,
+                    'bayar_infaq'           => $infaq,
+                    'data_peserta_id_infaq' => $peserta_id
+                ];
+                $this->infaq->insert($data_infaq);
+            }
 
-        //     if ($spp2 == '0') {
-        //         $spp2 = NULL;
-        //         $dt_konfirmasi_spp2 = NULL;
-        //     }
+            //Get data total bayar
+            $peserta_kelas      = $this->peserta_kelas->find($peserta_kelas_id);
+            $byr_daftar         = $peserta_kelas['byr_daftar'];
+            $byr_modul          = $peserta_kelas['byr_modul'];
+            $byr_spp1           = $peserta_kelas['byr_spp1'];
+            $byr_spp2           = $peserta_kelas['byr_spp2'];
+            $byr_spp3           = $peserta_kelas['byr_spp3'];
+            $byr_spp4           = $peserta_kelas['byr_spp4'];
 
-        //     if ($spp3 == '0') {
-        //         $spp3 = NULL;
-        //         $dt_konfirmasi_spp3 = NULL;
-        //     }
+            $beasiswa_daftar    = $peserta_kelas['beasiswa_daftar'];
+            $beasiswa_spp1      = $peserta_kelas['beasiswa_spp1'];
+            $beasiswa_spp2      = $peserta_kelas['beasiswa_spp2'];
+            $beasiswa_spp3      = $peserta_kelas['beasiswa_spp3'];
+            $beasiswa_spp4      = $peserta_kelas['beasiswa_spp4'];
 
-        //     if ($spp4 == '0') {
-        //         $spp4 = NULL;
-        //         $dt_konfirmasi_spp4 = NULL;
-        //     }
-
-        //     if ($lainnya != '0') {
-        //         $data_lain = [
-        //             'lainnya_bayar_id'        => $bayar_id,
-        //             'bayar_lainnya'           => $lainnya,
-        //             'data_peserta_id_lain'    => $peserta_id,
-        //             'status_bayar_lainnya'    => 'Lunas',
-        //         ];
-        //         $this->bayar_lain->insert($data_lain);
-        //     }
-
-        //     if ($modul != '0') {
-        //         $data_modul = [
-        //             'bayar_modul_id'        => $bayar_id,
-        //             'bayar_modul'           => $modul,
-        //             'status_bayar_modul'    => 'Lunas',
-        //         ];
-
-        //         $this->bayar_modul->insert($data_modul);
-        //     }
-
-        //     if ($infaq != '0') {
-        //         $data_infaq = [
-        //             'infaq_bayar_id'=> $bayar_id,
-        //             'bayar_infaq'   => $infaq,
-        //         ];
-        //         $this->infaq->insert($data_infaq);
-        //     }
-
-        //     if ($daftar != '0' && $spp1 != '0' && $spp2 != '0' && $spp3 != '0' && $spp4 != '0') {
-        //         $spp_status = 'LUNAS';
-        //     } else {
-        //         $spp_status = 'BELUM LUNAS';
-        //     }
+            $payments = [
+                [$byr_daftar, $beasiswa_daftar],
+                [$byr_spp1, $beasiswa_spp1],
+                [$byr_spp2, $beasiswa_spp2],
+                [$byr_spp3, $beasiswa_spp3],
+                [$byr_spp4, $beasiswa_spp4]
+            ];
             
+            $spp_status = 'LUNAS';
+            
+            foreach ($payments as $payment) {
+                if (($payment[0] == '0' && $payment[1] != 1) || ($payment[0] == NULL && $payment[1] != 1)) {
+                    $spp_status = 'BELUM LUNAS';
+                    break;
+                }
+            }
 
-        //     $dataabsen = [
-        //         'bckp_absen_peserta_id'     => $peserta_id,
-        //         'bckp_absen_peserta_kelas'  => $kelas_id,
-        //     ];
-        //     $this->absen_peserta->insert($dataabsen);
+            $PKstatus = [
+                'spp_status'  => $spp_status,
+            ];
+            $this->peserta_kelas->update($peserta_kelas_id, $PKstatus);
+            $this->cart->delete($cart_id);
+            $this->db->transComplete();
 
-        //     $dataujian = [
-        //         'bckp_ujian_peserta'     => $peserta_id,
-        //         'bckp_ujian_kelas'       => $kelas_id,
-        //     ];
-        //     $this->ujian->insert($dataujian);
+            $aktivitas = 'Pendaftaran peserta '. $peserta['nis'].'-'.$peserta['nama_peserta'] .' pada kelas ' . $data_kelas['nama_kelas']. ' terkonfirmasi oleh flip';
 
-        //     $updatePK = [
-        //         'data_absen'                => $this->absen_peserta->insertID(),
-        //         'data_ujian'                => $this->ujian->insertID(),
-        //         'byr_daftar'                => $daftar,
-        //         'byr_modul'                 => $modul,
-        //         'byr_spp1'                  => $spp1,
-        //         'byr_spp2'                  => $spp2,
-        //         'byr_spp3'                  => $spp3,
-        //         'byr_spp4'                  => $spp4,
-        //         'spp_status'                => $spp_status,
-        //         'dt_konfirmasi_daftar'      => $dt_konfirmasi_daftar,
-        //         'dt_konfirmasi_spp2'        => $dt_konfirmasi_spp2,
-        //         'dt_konfirmasi_spp3'        => $dt_konfirmasi_spp3,
-        //         'dt_konfirmasi_spp4'        => $dt_konfirmasi_spp4,
-        //         'expired_tgl_daftar'        => NULL,
-        //         'expired_waktu_daftar'      => NULL,
-        //     ];
-
-        //     $this->peserta_kelas->update($peserta_kelas_id, $updatePK);
-        //     $this->cart->delete($cart_id);
-        //     $this->db->transComplete();
-
-        //     $aktivitas = 'Pendaftaran peserta '. $peserta['nis'].'-'.$peserta['nama_peserta'] .' pada kelas ' . $data_kelas['nama_kelas']. ' terkonfirmasi oleh flip';
-
-        //     if ($this->db->transStatus() === FALSE)
-        //     {
-        //         /*--- Log ---*/
-        //         $log = [
-        //             'username_log' => 'Flip Payment',
-        //             'tgl_log'      => date("Y-m-d"),
-        //             'waktu_log'    => date("H:i:s"),
-        //             'status_log'   => 'FAIL',
-        //             'aktivitas_log'=> $aktivitas,
-        //         ];
-        //         $this->log->insert($log);
-        //     }
-        //     else
-        //     {
-        //         /*--- Log ---*/
-        //         $log = [
-        //             'username_log' => 'Flip Payment',
-        //             'tgl_log'      => date("Y-m-d"),
-        //             'waktu_log'    => date("H:i:s"),
-        //             'status_log'   => 'BERHASIL',
-        //             'aktivitas_log'=> $aktivitas,
-        //         ];
-        //         $this->log->insert($log);
-        //     }
-        //     return $this->respondCreated(['message' => 'Callback Success']);
-        // } else {
-        //     /*--- Log ---*/
-        //     $log = [
-        //         'username_log' => 'Flip Payment',
-        //         'tgl_log'      => date("Y-m-d"),
-        //         'waktu_log'    => date("H:i:s"),
-        //         'status_log'   => 'GAGAL',
-        //         'aktivitas_log'=> $bill_title.' '.$status.' invoice bill: '.$bill_title.', total: '.$amount,
-        //     ];
-        //     $this->log->insert($log);
-        //     return $this->fail('An error occurred');
-        // }
+            if ($this->db->transStatus() === FALSE)
+            {
+                /*--- Log ---*/
+                $log = [
+                    'username_log' => 'Flip Payment',
+                    'tgl_log'      => date("Y-m-d"),
+                    'waktu_log'    => date("H:i:s"),
+                    'status_log'   => 'FAIL',
+                    'aktivitas_log'=> $aktivitas,
+                ];
+                $this->log->insert($log);
+            }
+            else
+            {
+                /*--- Log ---*/
+                $log = [
+                    'username_log' => 'Flip Payment',
+                    'tgl_log'      => date("Y-m-d"),
+                    'waktu_log'    => date("H:i:s"),
+                    'status_log'   => 'BERHASIL',
+                    'aktivitas_log'=> $aktivitas,
+                ];
+                $this->log->insert($log);
+            }
+            return $this->respondCreated([
+                'message' => 'Callback Success',
+                'id' => $id,
+                'bill_link_id' => $bill_link_id,
+                'bill_link' => $bill_link,
+                'bill_title' => $bill_title,
+                'sender_name' => $sender_name,
+                'sender_email' => $sender_email,
+                'sender_bank' => $sender_bank,
+                'sender_bank_type' => $sender_bank_type,
+                'amount' => $amount,
+                'status' => $status,
+                'created_at' => $created_at,
+            ]);
+        } else {
+            /*--- Log ---*/
+            $log = [
+                'username_log' => 'Flip Payment',
+                'tgl_log'      => date("Y-m-d"),
+                'waktu_log'    => date("H:i:s"),
+                'status_log'   => 'GAGAL',
+                'aktivitas_log'=> $bill_title.' '.$status.' invoice bill: '.$bill_title.', total: '.$amount,
+            ];
+            $this->log->insert($log);
+            return $this->respondCreated(['message' => 'Callback Failed']);
+        }
+        
     }
     
 }
